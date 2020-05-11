a06d731164fb98a60f6a9b425fb41b660acd135a
DillonKern <60273421+DillonKern@users.noreply.github.com>
2020-05-11 11:25:36 -0500

    init commit

---
+++ dataStructures/HW2/cmake-build-debug/CMakeFiles/Homework2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework2.dir/PhysVec.cpp.o"
+  "CMakeFiles/Homework2.dir/main.cpp.o"
+  "Homework2.exe"
+  "Homework2.pdb"
+  "libHomework2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
